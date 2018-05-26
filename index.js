32e5d02174899b7cf8cf104bdf69b9b0a58fe3c5
Billy Janitsch <billy@kensho.com>
2018-05-25 21:13:34 -0400

    Simplify

--- index.js
+++ index.js
@@ -6,19 +6,20 @@ const webpack = require('webpack')
 function parse(pkg) {
   const raw = pkg.split('@')
   if (raw.length < 1) throw new Error()
-  const name = raw[0]
-  const version = raw.length > 1 ? raw[1] : 'latest'
-  return {name, version}
+  return raw[0]
 }
 
-function install(pkg, cwd) {
-  const {name, version} = parse(pkg)
-  return execa('npm', ['install', `${name}@${version}`], {cwd})
+function install(pkg, dir) {
+  return execa('npm', ['install', pkg], {cwd: dir})
 }
 
-function build(entry, context) {
-  const outputDir = path.resolve(context, 'dist')
-  const config = {context, entry, mode: 'production', output: {path: outputDir}}
+function build(pkg, dir) {
+  const config = {
+    mode: 'production',
+    context: dir,
+    entry: pkg,
+    output: {path: path.resolve(dir, 'dist')}
+  }
   return new Promise((resolve, reject) => {
     webpack(config, (error, stats) => {
       if (error) reject(error)
@@ -32,7 +33,7 @@ async function bundleSizer(pkg, dependencies = []) {
   const cwd = await tempy.directory()
   for (const dep of dependencies) await install(dep, cwd)
   await install(pkg, cwd)
-  const {name} = parse(pkg)
+  const name = parse(pkg)
   return build(name, cwd)
 }
 
